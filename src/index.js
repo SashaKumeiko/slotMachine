ec222263b34c439696796b56978e60592239d3ed
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-09-11 14:31:15 +0300

    add animation and disabled state of button during animation

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-import './style.css'
-import imgPine from './assets/SYM4.png'
-import imgLemon from './assets/SYM5.png'
-import imgApple from './assets/SYM6.png'
-import imgStrawberry from './assets/SYM3.png'
-import imgGrapes from './assets/SYM7.png'
-import imgWild from './assets/SYM1.png'
-import backImg from './assets/BG.png'
-import spinImg from './assets/BTN_Spin.png'
+import './style.css';
+import imgPine from './assets/SYM4.png';
+import imgLemon from './assets/SYM5.png';
+import imgApple from './assets/SYM6.png';
+import imgStrawberry from './assets/SYM3.png';
+import imgGrapes from './assets/SYM7.png';
+import imgWild from './assets/SYM1.png';
+import backImg from './assets/BG.png';
+import spinGrayImg from './assets/BTN_Spin_d.png';
+import spinImg from './assets/BTN_Spin.png';
 let symbols = [
   {
     name: 'pineapple',
-    image: imgPine
+    image: imgPine,
   },
   {
     name: 'strawberry',
-    image: imgStrawberry
+    image: imgStrawberry,
   },
   {
     name: 'lemon',
-    image: imgLemon
+    image: imgLemon,
   },
   {
     name: 'grapes',
-    image: imgGrapes
+    image: imgGrapes,
   },
   {
     name: 'apple',
-    image: imgApple
+    image: imgApple,
   },
   {
     name: 'wild',
-    image: imgWild
+    image: imgWild,
   },
 ];
 
@@ -40,7 +41,7 @@ class Game {
       return Game.instance;
     }
     this.cash = 100;
-    this.win = ''
+    this.win = '';
     Game.instance = this;
     return this;
   }
@@ -52,32 +53,61 @@ class Game {
     return (this.cash -= 10);
   }
 
+  toggleStartButton() {
+    let button = document.querySelector('.start-button');
+    if (button.disabled) {
+      button.disabled = false;
+      button.style.backgroundImage = `url(${spinImg})`;
+    } else {
+      button.disabled = true;
+      button.style.backgroundImage = `url(${spinGrayImg})`;
+    }
+    console.log(button);
+  }
+
   start() {
-    // console.log('start');
+    this.toggleStartButton();
     let gridContainer = document.querySelector('.grid-container');
     function generate() {
       let div = document.createElement('div');
       div.classList.add('container');
       for (let i = 1; i <= 9; i++) {
         let slot = document.createElement('div');
-        let symbol = symbols[Math.floor(Math.random() * Math.floor(6))]
-        slot.id = `${
-          symbol.name
-        }`
+        let symbol = symbols[Math.floor(Math.random() * Math.floor(6))];
+        slot.id = `${symbol.name}`;
         slot.classList.add('slot');
         slot.classList.add(`slot${i}`);
-        slot.innerHTML = `<img src=${symbol.image} class='slotImg'>`
+
+        switch (i) {
+          case 1:
+          case 4:
+          case 7:
+            slot.classList.add('animation1');
+            break;
+          case 2:
+          case 5:
+          case 8:
+            slot.classList.add('animation2');
+            break;
+          case 3:
+          case 6:
+          case 9:
+            slot.classList.add('animation3');
+            break;
+        }
+
+        slot.innerHTML = `<img src=${symbol.image} class='slotImg'>`;
+
         div.append(slot);
       }
-      //   console.log(div);
       return div;
     }
     gridContainer.innerHTML = generate().innerHTML;
-    Game.checkWin();
+    this.checkWin();
+    let timeout = setTimeout(this.toggleStartButton, 1000);
   }
 
-  static checkWin() {
-    //   console.log('checkwin')
+  checkWin() {
     const slot1 = document.querySelector('.slot1');
     const slot2 = document.querySelector('.slot2');
     const slot3 = document.querySelector('.slot3');
@@ -88,7 +118,6 @@ class Game {
     const slot8 = document.querySelector('.slot8');
     const slot9 = document.querySelector('.slot9');
 
- 
     let arr1 = [slot1.id, slot2.id, slot3.id];
     let arr2 = [slot4.id, slot5.id, slot6.id];
     let arr3 = [slot7.id, slot8.id, slot9.id];
@@ -97,14 +126,11 @@ class Game {
 
     // }
     let result = '';
-    //   let arrTest = [ 'pineapple', 'pineapple','pineapple']
-    //   console.log(arrTest)
-    // let arraysTest =[arrTest]
+
     this.win = arrays.some((arr) => {
       let line = arr.join('');
       let arrSeparatedByWild = line.split('wild');
       if (arrSeparatedByWild.length == 3) {
-        // console.log('win');
         result = 'win';
         return true;
       } else if (arrSeparatedByWild.length == 2) {
@@ -115,27 +141,24 @@ class Game {
           case 0:
             console.log('wew');
             if (arr[1] === arr[2]) {
-              // console.log('win');
               result = 'win';
               return true;
             } else result = 'lost';
             break;
           case 1:
             if (arr[0] === arr[2]) {
-              // console.log('win');
               result = 'win';
               return true;
             } else result = 'lost';
             break;
           case 2:
             if (arr[0] === arr[1]) {
-              // console.log('win');
               result = 'win';
               return true;
             } else result = 'lost';
             break;
         }
-      } else if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0]!='wild') {
+      } else if (arr[0] === arr[1] && arr[0] === arr[2] && arr[0] != 'wild') {
         console.log('win 3 same');
         result = 'win';
         return true;
@@ -144,7 +167,6 @@ class Game {
       }
     });
     console.log(result, this.win);
-
   }
 
   loading() {
@@ -162,25 +184,23 @@ class Game {
       <div class="slot slot8"><img src = ${symbols[3].image} class='slotImg'></div>    
       <div class="slot slot9"><img src = ${symbols[0].image} class='slotImg'></div>    
       </div>
-      <div class='start-button'></div>
+      <button class='start-button'></button>
       </div>
       `;
-      const container = document.querySelector('.container')
-      container.style.backgroundImage = `url(${backImg})`
+    const container = document.querySelector('.container');
+    container.style.backgroundImage = `url(${backImg})`;
 
-const startButton =document.querySelector('.start-button')
-      startButton.style.backgroundImage = `url(${spinImg})`
-      startButton.addEventListener('click', this.start);
+    const startButton = document.querySelector('.start-button');
+    startButton.style.backgroundImage = `url(${spinImg})`;
+    startButton.addEventListener('click', this.start.bind(this));
 
     return;
   }
 }
 
-let A = new Game();
-let B = new Game();
-// A.spendCash();
-// A.spendCash();
-// B.spendCash();
-// console.log(A);
-A.loading();
-// console.log(A.getCash());
+setTimeout(() => {
+  document.querySelector('.initial').classList.remove('initial');
+
+  let A = new Game();
+  A.loading();
+}, 500);
