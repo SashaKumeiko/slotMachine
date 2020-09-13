fec54732e6746504d38bce31f1d7e951257d6a8a
Sasha Kumeiko <sashaqmeyko@gmail.com>
2020-09-13 21:29:59 +0300

    add modal, add div that renders stats

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import './modal.css';
 
 class Game {
   constructor(data) {
@@ -26,7 +27,6 @@ class Game {
     let button = document.querySelector('.start-button');
     if (button.disabled) {
       button.disabled = false;
-      console.log('this', this.data.spins[0].image);
       button.style.backgroundImage = `url(${this.data.spins[0].image})`;
     } else {
       button.disabled = true;
@@ -38,18 +38,16 @@ class Game {
     clearTimeout(timeout);
     this.toggleStartButton();
     let gridContainer = document.querySelector('.grid-container');
-    const generate = () => {
-      console.log(this);
 
+    const generate = () => {
       let div = document.createElement('div');
-      div.classList.add('container');
+      // div.classList.add('container');
       for (let i = 1; i <= 9; i++) {
         let slot = document.createElement('div');
         let symbol = this.data.slots[Math.floor(Math.random() * Math.floor(6))];
         slot.id = `${symbol.name}`;
         slot.classList.add('slot');
         slot.classList.add(`slot${i}`);
-
         switch (i) {
           case 1:
           case 4:
@@ -67,13 +65,12 @@ class Game {
             slot.classList.add('animation3');
             break;
         }
-
         slot.innerHTML = `<img src=${symbol.image} class='slotImg'>`;
-
         div.append(slot);
       }
       return div;
     };
+
     gridContainer.innerHTML = generate().innerHTML;
     this.checkWin();
     let timeout = setTimeout(this.toggleStartButton.bind(this), 1000);
@@ -95,7 +92,6 @@ class Game {
     let arr3 = [slot7.id, slot8.id, slot9.id];
     let arrays = [arr1, arr2, arr3];
 
-    // }
     let result = '';
 
     this.win = arrays.some((arr) => {
@@ -105,12 +101,9 @@ class Game {
         result = 'win';
         return true;
       } else if (arrSeparatedByWild.length == 2) {
-        console.log('hey');
         let index = arr.findIndex((el) => el === 'wild');
-        console.log(index);
         switch (index) {
           case 0:
-            console.log('wew');
             if (arr[1] === arr[2]) {
               result = 'win';
               return true;
@@ -137,13 +130,51 @@ class Game {
         result = 'lost';
       }
     });
+    if (this.win){ 
+      this.amountOfWins++;
+      this.showWin();
+      this.coins+=10;
+    }
+    else{
+      this.coins-=5;
+    }
+
     console.log(result, this.win);
+    if(this.coins<=0) {this.coins==0; 
+    this.toggleStartButton();}
+    this.setStats()
+    console.log("coins:",this.coins,"wins:",this.amountOfWins)
+  }
+  setStats(){
+    document.querySelector('.stats').innerHTML=`
+    <div>Money: ${this.coins}</div>
+    <div>Wins: ${this.amountOfWins} </div>`
+  }
+  showWin() {
+    clearTimeout(modalTimeout);
+
+    document.querySelector('.modal').style.display = 'block';
+    const modal = document.getElementById('myModal');
+    const modalContent = document.querySelector('.modal-content');
+    const modalText = document.querySelector('.modal-content div');
+    const container = document.querySelector('.container');
+
+    window.onclick = function (event) {
+      if (
+        event.target == modalContent ||
+        event.target == container ||
+        event.target == modalText
+      ) {
+        modal.style.display = 'none';
+      }
+    };
+    const modalTimeout = setTimeout(() => {
+      modal.style.display = 'none';
+    }, 3000);
   }
 
-  showWin() {}
-
   loading() {
-    clearTimeout(timeLoading)
+    clearTimeout(timeLoading);
     console.log(this.data.apple);
     let root = document.getElementById('root');
     root.innerHTML = `
@@ -160,6 +191,10 @@ class Game {
       <div class="slot slot9"><img src = ${this.data.slots[0].image} class='slotImg'></div>    
       </div>
       <button class='start-button'></button>
+      <div class='stats'>
+      <div>Money: 100</div>
+      <div>Wins: 0 </div>
+      </div>
       </div>
       `;
     const container = document.querySelector('.container');
